3a46b7a4f9aad6c2ea8b2cda1f91e87552219e4f
mhd-amirul <amirulm236@gmail.com>
2023-04-12 14:25:57 +0800

    feat: upgrade to laravel 10

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\BarangController;
-use App\Http\Controllers\Admin\GalleryController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,35 +8,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/product/{slug}', [HomeController::class, 'show'])->name('product.show');
-
-Route::group(['middleware' => 'auth'], function() {
-
-    // Proses Admin
-    Route::group(['prefix' => 'admin2'], function() {
-        Route::resource('barang', BarangController::class);
-        Route::resource('gallery', GalleryController::class);
-    });
-
-    // Proses User
-    Route::group([], function () {
-        Route::post('/card/{id}', [HomeController::class, 'addCard'])->name('card.add');
-        Route::get('/card', [HomeController::class, 'listCard'])->name('card.list');
-    });
-});
-
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
