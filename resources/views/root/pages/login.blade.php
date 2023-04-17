fc929992c5cbc054e5e4e02af5a9ddb27542110b
mhd-amirul <amirulm236@gmail.com>
2023-04-17 12:33:55 +0800

    fix: resource view

--- resources/views/root/pages/login.blade.php
+++ resources/views/root/pages/login.blade.php
@@ -21,26 +21,48 @@
     <div class="container">
         <div class="row">
         <div class="col-lg-6 offset-lg-3">
+            @if (Session::has("session_success"))
+                <div class="alert alert-success" role="alert">
+                    <li>{{ Session::get("session_success") }}</li>
+                </div>
+                <br><br>
+            @endif
+            @if (Session::has("session_errors"))
+                <div class="alert alert-danger" role="alert">
+                    <li>{{ Session::get("session_errors") }}</li>
+                </div>
+                <br><br>
+            @endif
+            @if ($errors->any())
+                <div class="alert alert-danger" role="alert">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </div>
+                <br><br>
+            @endif
             <div class="login-form">
             <h2>Login</h2>
-            <form action="#">
+            <form action="{{ route("signin.check") }}" method="POST">
+                @csrf
+                @method("post")
                 <div class="group-input">
-                <label for="username">Username or email address *</label>
-                <input type="text" id="username" />
+                    <label for="email">Email address *</label>
+                    <input type="text" id="email" name="email" value="{{ old('email') }}"/>
                 </div>
                 <div class="group-input">
-                <label for="pass">Password *</label>
-                <input type="text" id="pass" />
+                    <label for="pass">Password *</label>
+                    <input type="text" id="pass" name="pass"/>
                 </div>
                 <div class="group-input gi-check">
-                <div class="gi-more">
-                    <label for="save-pass">
-                    Save Password
-                    <input type="checkbox" id="save-pass" />
-                    <span class="checkmark"></span>
-                    </label>
-                    <a href="#" class="forget-pass">Forget your Password</a>
-                </div>
+                    <div class="gi-more">
+                        <label for="save-pass">
+                            Save Password
+                            <input type="checkbox" id="save-pass" name="remember" />
+                            <span class="checkmark"></span>
+                        </label>
+                        <a href="#" class="forget-pass">Forget your Password</a>
+                    </div>
                 </div>
                 <button type="submit" class="site-btn login-btn">
                 Sign In
