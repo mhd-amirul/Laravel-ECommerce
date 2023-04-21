9c537acd48f6a881293b692646bd71ebc36614b3
mhd-amirul <amirulm236@gmail.com>
2023-04-22 07:46:59 +0800

    fix: link click tag a to form

--- resources/views/root/includes/js.blade.php
+++ resources/views/root/includes/js.blade.php
@@ -9,3 +9,19 @@
 <script src="{{ url('js/jquery.slicknav.js') }}"></script>
 <script src="{{ url('js/owl.carousel.min.js') }}"></script>
 <script src="{{ url('js/main.js') }}"></script>
+
+<script>
+    function submitForm() {
+        let form = document.getElementById("form__submit");
+        form.submit();
+    }
+
+    function submitButton() {
+        let button = document.getElementById("button__submit");
+        button.click();
+    }
+
+    $("log_out_user_link").click(function() {
+        document.getElementById("log_out_user").submit();
+    })
+</script>
