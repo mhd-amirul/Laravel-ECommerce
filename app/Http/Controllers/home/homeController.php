1fd9300572b02ffa26c332a2eb60c60edfce3286
mhd-amirul <amirulm236@gmail.com>
2023-04-22 08:07:41 +0800

    fix: get data product from db

--- app/Http/Controllers/home/homeController.php
+++ app/Http/Controllers/home/homeController.php
@@ -3,28 +3,48 @@
 namespace App\Http\Controllers\home;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Services\Interfaces\GeneralServiceInterface;
 use Illuminate\Http\Request;
 
 class homeController extends Controller
 {
-    public $basic = [
-        "shop_email"   => "uniform_shop@gmail.com",
-        "shop_number"  => "+65 11.188.888",
-        "shop_address" => "60-49 Road 11378 New York",
-    ];
+    protected $basic = [];
+    protected $generalService;
+
+    public function __construct(GeneralServiceInterface $generalService)
+    {
+        $this->generalService = $generalService;
+        $this->basic          = $this->generalService->basic_item();
+    }
 
     public function go_to_index()
     {
-        return view("root.pages.index")->with("basic", $this->basic);
+        $product  = Product::latest()->whereNot("discount", 50)->get();
+        $discount = Product::latest()->where("discount", 50)->get();
+
+        return view("root.pages.index")
+            ->with([
+                "basic"   => $this->basic,
+                "title"   => "home",
+                "products" => $product,
+                "discount" => $discount
+            ]);
     }
 
     public function go_to_product()
     {
-        return view("root.pages.product")->with("basic", $this->basic);
+        return view("root.pages.product")->with([
+            "basic"      => $this->basic,
+            "title"      => "detail product",
+            "breadcrumb" => [["route" => "product", "name" => "Detail Product"]]]);
     }
 
     public function go_to_shop_card()
     {
-        return view("root.pages.shopping-cart")->with("basic", $this->basic);
+        return view("root.pages.shopping-cart")->with([
+            "basic"      => $this->basic,
+            "title"      => "shopping cart",
+            "breadcrumb" => [["route" => "shopping-card", "name" => "Shopping Cart"]]]);
     }
 }
