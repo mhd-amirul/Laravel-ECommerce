7f6ac0dbaada2a20295611bc86b4c78c28fe550e
mhd-amirul <amirulm236@gmail.com>
2023-04-22 08:06:25 +0800

    fix: set up basic data on view

--- resources/views/root/pages/product.blade.php
+++ resources/views/root/pages/product.blade.php
@@ -1,22 +1,6 @@
 @extends('root.layouts.user_main')
 
 @section('content')
-
-    <!-- Breadcrumb Section Begin -->
-    <div class="breacrumb-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="breadcrumb-text product-more">
-              <a href="./home.html"><i class="fa fa-home"></i> Home</a>
-              <span>Detail</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Breadcrumb Section Begin -->
-
     <!-- Product Shop Section Begin -->
     <section class="product-shop spad page-details">
       <div class="container">
