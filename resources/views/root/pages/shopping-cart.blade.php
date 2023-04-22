d09d995612d2ccecb0a7bbfd1bfed5772073f6e9
mhd-amirul <amirulm236@gmail.com>
2023-04-22 08:06:42 +0800

    fix: set up basic data on view

--- resources/views/root/pages/shopping-cart.blade.php
+++ resources/views/root/pages/shopping-cart.blade.php
@@ -1,22 +1,6 @@
 @extends('root.layouts.user_main')
 
 @section('content')
-
-    <!-- Breadcrumb Section Begin -->
-    <div class="breacrumb-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="breadcrumb-text product-more">
-              <a href="./home.html"><i class="fa fa-home"></i> Home</a>
-              <span>Shopping Cart</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Breadcrumb Section Begin -->
-
     <!-- Shopping Cart Section Begin -->
     <section class="shopping-cart spad">
       <div class="container">
