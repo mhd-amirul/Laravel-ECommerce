6ec3b5d9c79db6ce17845c74cfd7b96dc5bbfd5b
mhd-amirul <amirulm236@gmail.com>
2023-04-23 10:23:14 +0800

    fix: bind generalService with interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\GeneralService;
+use App\Services\Interfaces\GeneralServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(GeneralServiceInterface::class, GeneralService::class);
     }
 
     /**
