4e398aabfc5acf09e729d41df737e2b07daac52c
mhd-amirul <amirulm236@gmail.com>
2023-05-08 15:17:16 +0800

    fix: get data from db

--- resources/views/root/pages/product.blade.php
+++ resources/views/root/pages/product.blade.php
@@ -9,52 +9,30 @@
             <div class="row">
               <div class="col-lg-6">
                 <div class="product-pic-zoom">
-                  <img
-                    class="product-big-img"
-                    src="{{ url('images/product-single/product-1.jpg') }}"
-                    alt=""
-                  />
+                  <img class="product-big-img" src="{{ asset('storage/'.$product->image) }}" alt="" />
                   <div class="zoom-icon">
                     <i class="fa fa-search-plus"></i>
                   </div>
                 </div>
                 <div class="product-thumbs">
                   <div class="product-thumbs-track ps-slider owl-carousel">
-                    <div
-                      class="pt active"
-                      data-imgbigurl="{{ url('images/product-single/product-1.jpg') }}"
-                    >
-                      <img src="{{ url('images/product-single/product-1.jpg') }}" alt="" />
+                    <div class="pt active" data-imgbigurl="{{ asset('storage/'.$product->image) }}" >
+                      <img src="{{ asset('storage/'.$product->image) }}" alt="" />
                     </div>
 
-                    <div
-                      class="pt"
-                      data-imgbigurl="{{ url('images/product-single/product-2.jpg') }}"
-                    >
-                      <img src="{{ url('images/product-single/product-2.jpg') }}" alt="" />
-                    </div>
-
-                    <div
-                      class="pt"
-                      data-imgbigurl="{{ url('images/product-single/product-3.jpg') }}"
-                    >
-                      <img src="{{ url('images/product-single/product-3.jpg') }}" alt="" />
-                    </div>
-
-                    <div
-                      class="pt"
-                      data-imgbigurl="{{ url('images/product-single/product-3.jpg') }}"
-                    >
-                      <img src="{{ url('images/product-single/product-3.jpg') }}" alt="" />
-                    </div>
+                    {{-- @foreach ($collection as $item) --}}
+                      <div class="pt" data-imgbigurl="{{ asset('storage/'.$product->image) }}" >
+                        <img src="{{ asset('storage/'.$product->image) }}" alt="" />
+                      </div>
+                    {{-- @endforeach --}}
                   </div>
                 </div>
               </div>
               <div class="col-lg-6">
                 <div class="product-details">
                   <div class="pd-title">
-                    <span>oranges</span>
-                    <h3>Pure Pineapple</h3>
+                    <span>{{ $product->category }}</span>
+                    <h3>{{ $product->name }}</h3>
                     <a href="#" class="heart-icon"
                       ><i class="icon_heart_alt"></i
                     ></a>
@@ -69,34 +47,22 @@
                   </div>
                   <div class="pd-desc">
                     <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Corporis, error officia. Rem aperiam laborum voluptatum
-                      vel, pariatur modi hic provident eum iure natus quos non a
-                      sequi, id accusantium! Autem.
+                      {{ $product->description }}
                     </p>
-                    <p>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                      Quam possimus quisquam animi, commodi, nihil voluptate
-                      nostrum neque architecto illo officiis doloremque et
-                      corrupti cupiditate voluptatibus error illum. Commodi
-                      expedita animi nulla aspernatur. Id asperiores blanditiis,
-                      omnis repudiandae iste inventore cum, quam sint molestiae
-                      accusamus voluptates ex tempora illum sit perspiciatis.
-                      Nostrum dolor tenetur amet, illo natus magni veniam quia
-                      sit nihil dolores. Commodi ratione distinctio harum
-                      voluptatum velit facilis voluptas animi non laudantium, id
-                      dolorem atque perferendis enim ducimus? A exercitationem
-                      recusandae aliquam quod. Itaque inventore obcaecati, unde
-                      quam impedit praesentium veritatis quis beatae ea atque
-                      perferendis voluptates velit architecto?
-                    </p>
-                    <h4>$495.00 <span>629.99</span></h4>
+                    <h4>${{ $product->price }}.00 
+                      {{-- <span>629.99</span> --}}
+                    </h4>
                   </div>
                   <div class="quantity">
                     <div class="pro-qty">
                       <input type="text" value="1" />
+                      <form action="{{ route("insert.cart") }}" method="POST" id="insert__cart" hidden>
+                        @csrf
+                        <input type="text" value="1" id="quantity" name="quantity"/>
+                        <input type="text" value="{{ $product->id }}" id="product_id" name="product_id"/>
+                      </form>
                     </div>
-                    <a href="#" class="primary-btn pd-cart">Add To Cart</a>
+                    <a class="primary-btn pd-cart" onclick="submitForm('insert__cart')">Add To Cart</a>
                   </div>
                 </div>
               </div>
@@ -118,10 +84,51 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-lg-3 col-sm-6">
+          <div class="col-lg-12 col-sm-5">
+            <div class="product-slider owl-carousel">
+              @if (isset($related))
+                  @foreach ($related as $item)
+                    <div class="product-item">
+                        <div class="pi-pic">
+                        <img src="{{ asset('storage/'.$item->image) }}" alt="" height="427" width="380"/>
+                        <div class="sale">Sale</div>
+                        <div class="icon">
+                            <i class="icon_heart_alt"></i>
+                        </div>
+                        <form action="{{ route("insert.cart") }}" method="POST" id="insert__cart_related{{ $item->id }}" hidden>
+                          @csrf
+                          <input type="text" value="1" id="quantity" name="quantity"/>
+                          <input type="text" value="{{ $item->id }}" id="product_id" name="product_id"/>
+                      </form>
+                        <ul>
+                            <li class="w-icon active">
+                            <a onclick="submitForm('insert__cart_related{{ $item->id }}')"><i class="icon_bag_alt"></i></a>
+                            </li>
+                            <li class="quick-view"><a href="{{ route("product")."?product=".$item->id }}">+ Quick View</a></li>
+                            {{-- <li class="w-icon">
+                            <a href="#"><i class="fa fa-random"></i></a>
+                            </li> --}}
+                        </ul>
+                        </div>
+                        <div class="pi-text">
+                        <div class="catagory-name">{{ $item->category }}</div>
+                        <a href="#">
+                            <h5>{{ $item->name }}</h5>
+                        </a>
+                        <div class="product-price">
+                            ${{ $item->price - 5 }}.00
+                            <span>${{ $item->price }}.00</span>
+                        </div>
+                        </div>
+                    </div>
+                  @endforeach
+              @endif
+            </div>
+          </div>
+          {{-- <div class="col-lg-3 col-sm-6">
             <div class="product-item">
               <div class="pi-pic">
-                <img src="{{ url('images/products/women-1.jpg') }}" alt="" />
+                <img src="{{ asset('storage/'.$item->image) }}" alt="" />
                 <div class="sale">Sale</div>
                 <div class="icon">
                   <i class="icon_heart_alt"></i>
@@ -130,108 +137,21 @@
                   <li class="w-icon active">
                     <a href="#"><i class="icon_bag_alt"></i></a>
                   </li>
-                  <li class="quick-view"><a href="#">+ Quick View</a></li>
-                  <li class="w-icon">
-                    <a href="#"><i class="fa fa-random"></i></a>
-                  </li>
+                  <li class="quick-view"><a href="{{ route("product")."?product=".$item->id }}">+ Quick View</a></li>
                 </ul>
               </div>
               <div class="pi-text">
-                <div class="catagory-name">Coat</div>
+                <div class="catagory-name">{{ $item->category }}</div>
                 <a href="#">
-                  <h5>Pure Pineapple</h5>
+                  <h5>{{ $item->name }}</h5>
                 </a>
                 <div class="product-price">
-                  $14.00
-                  <span>$35.00</span>
+                  ${{ $item->price - 5 }}.00
+                  <span>${{ $item->price }}.00</span>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="col-lg-3 col-sm-6">
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="{{ url('images/products/women-2.jpg') }}" alt="" />
-                <div class="icon">
-                  <i class="icon_heart_alt"></i>
-                </div>
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
-                  </li>
-                  <li class="quick-view"><a href="#">+ Quick View</a></li>
-                  <li class="w-icon">
-                    <a href="#"><i class="fa fa-random"></i></a>
-                  </li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Shoes</div>
-                <a href="#">
-                  <h5>Guangzhou sweater</h5>
-                </a>
-                <div class="product-price">
-                  $13.00
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-3 col-sm-6">
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="{{ url('images/products/women-3.jpg') }}" alt="" />
-                <div class="icon">
-                  <i class="icon_heart_alt"></i>
-                </div>
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
-                  </li>
-                  <li class="quick-view"><a href="#">+ Quick View</a></li>
-                  <li class="w-icon">
-                    <a href="#"><i class="fa fa-random"></i></a>
-                  </li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Towel</div>
-                <a href="#">
-                  <h5>Pure Pineapple</h5>
-                </a>
-                <div class="product-price">
-                  $34.00
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-3 col-sm-6">
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="{{ url('images/products/women-4.jpg') }}" alt="" />
-                <div class="icon">
-                  <i class="icon_heart_alt"></i>
-                </div>
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
-                  </li>
-                  <li class="quick-view"><a href="#">+ Quick View</a></li>
-                  <li class="w-icon">
-                    <a href="#"><i class="fa fa-random"></i></a>
-                  </li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Towel</div>
-                <a href="#">
-                  <h5>Converse Shoes</h5>
-                </a>
-                <div class="product-price">
-                  $34.00
-                </div>
-              </div>
-            </div>
-          </div>
+          </div> --}}
         </div>
       </div>
     </div>
