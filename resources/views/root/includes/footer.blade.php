9f3241a53cd038d42eaa951288bc8e5e3e0b8780
mhd-amirul <amirulm236@gmail.com>
2023-05-12 22:18:43 +0800

    feat: social media link and contact feature

--- resources/views/root/includes/footer.blade.php
+++ resources/views/root/includes/footer.blade.php
@@ -13,10 +13,10 @@
               <li>Email: {{ $basic["shop_email"] }}</li>
             </ul>
             <div class="footer-social">
-              <a href="#"><i class="fa fa-facebook"></i></a>
-              <a href="#"><i class="fa fa-instagram"></i></a>
-              <a href="#"><i class="fa fa-twitter"></i></a>
-              <a href="#"><i class="fa fa-pinterest"></i></a>
+              <a href="https://www.facebook.com/profile.php?id=100008272813247"><i class="fa fa-facebook"></i></a>
+              <a href="https://www.instagram.com/amirul_m236/"><i class="fa fa-instagram"></i></a>
+              <a href="https://twitter.com/"><i class="fa fa-twitter"></i></a>
+              <a href="https://id.pinterest.com"><i class="fa fa-pinterest"></i></a>
             </div>
           </div>
         </div>
@@ -24,9 +24,8 @@
           <div class="footer-widget">
             <h5>Information</h5>
             <ul>
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Contact</a></li>
-              <li><a href="#">Service</a></li>
+              <li><a href="https://mhd-amirul.github.io/MyPortfolio/">About Us</a></li>
+              <li><a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">Contact</a></li>
             </ul>
           </div>
         </div>
@@ -34,7 +33,7 @@
           <div class="footer-widget">
             <h5>My Account</h5>
             <ul>
-              <li><a href="#">My Account</a></li>
+              <li><a href="{{ route("profile") }}">My Account</a></li>
               <li><a href="{{ route('shopping.cart') }}">Shopping Cart</a></li>
             </ul>
           </div>
@@ -61,3 +60,34 @@
     </div>
   </footer>
   <!-- Footer Section End -->
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Contact Us</h5>
+      </div>
+      <div class="modal-body">
+        <form action="{{ route("contact") }}" method="POST" id="contactForm">
+          @csrf
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">email:</label>
+            <input type="email" class="form-control" id="recipient-name" name="email">
+          </div>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">name:</label>
+            <input type="text" class="form-control" id="recipient-name" name="name">
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Message:</label>
+            <textarea class="form-control" id="message-text" name="message"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" onclick="submitForm('contactForm')">Send message</button>
+      </div>
+    </div>
+  </div>
+</div>
