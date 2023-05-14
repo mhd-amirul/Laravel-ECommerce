6047fce1ab6e273551745103be8a052969f1e9e9
mhd-amirul <amirulm236@gmail.com>
2023-05-14 10:29:03 +0800

    fix: refactoring homeController

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -10,32 +10,32 @@ use App\Models\Product;
 use App\Models\Rating;
 use App\Models\Resource;
 use App\Services\Interfaces\GeneralServiceInterface;
+use App\Services\Interfaces\ProductServiceInterface;
 use Illuminate\Http\Request;
 
 class homeController extends Controller
 {
     protected $basic = [];
-    protected $product;
+    protected $productService;
     protected $generalService;
 
-    public function __construct(GeneralServiceInterface $generalService)
+    public function __construct(GeneralServiceInterface $generalService, ProductServiceInterface $productService)
     {
         $this->generalService = $generalService;
         $this->basic          = $this->generalService->basicItem();
-        $this->product        = new Product();
+        $this->productService = $productService;
     }
 
     public function goToIndex()
     {
-        $product  = $this->product->latest()->where("discount", '<' ,50)->get();
-        $discount = $this->product->latest()->where("discount", '>=' ,50)->get();
+        $product  = $this->productService->getProductDiscount();
 
         return view("root.pages.index")
             ->with([
                 "basic"     => $this->basic,
                 "title"     => "home",
-                "products"  => $product,
-                "discount"  => $discount,
+                "products"  => $product["product"],
+                "discount"  => $product["discount"],
                 "instagram" => Resource::where("group", "instagram")->get()
             ]);
     }
@@ -45,23 +45,19 @@ class homeController extends Controller
         if (!request()->has("product"))
             return redirect()->route("index");
 
-        $product = $this->product->where("id", request("product"))->with("Ratings")->first();
-        $related = $this->product->byCategory($this->product, explode(',', str_replace(' ', '', $product->category)))->get();
-
-        if ($product->Ratings->count() != 0)
-            $product->rating = $product->Ratings->sum("rating") / $product->Ratings->count();
+        $product = $this->productService->getInformationItem(request("product"));
 
         return view("root.pages.product")->with([
             "basic"      => $this->basic,
             "title"      => "detail product",
-            "product"    => $product,
-            "related"    => $related,
+            "product"    => $product["product"],
+            "related"    => $product["related"],
             "breadcrumb" => [["route" => "product", "name" => "Detail Product"]]]);
     }
 
     public function saveMessage(contactRequest $request)
     {
-        $contact = contact::create([
+        $contact = $this->generalService->sendMessage([
             "from"      => $request->email,
             "name"      => $request->name,
             "message"   => $request->message
